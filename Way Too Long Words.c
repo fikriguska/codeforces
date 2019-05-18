ea190e012e91eea472c2f651fba04bd1c457903d
fikriguska <31087829+fikriguska@users.noreply.github.com>
2019-05-18 17:03:13 +0700

    Create Way Too Long Words.c

---
+++ Way Too Long Words.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+	int n, i;
+	
+	scanf("%d", &n);
+	
+	char in[n][1000];
+	
+	for(i=0; i<n; i++){
+		scanf("%s", in[i]);
+	}
+	
+	for(i=0; i<n; i++){
+		if(strlen(in[i])>10){
+			printf("%c", in[i][0]);
+			printf("%d", strlen(in[i])-2);
+			printf("%c", in[i][strlen(in[i])-1]);
+		}
+		else
+			printf("%s", in[i]);
+		printf("\n");
+	}
+}
